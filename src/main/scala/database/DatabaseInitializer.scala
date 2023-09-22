e8ca5daccc0fb14edc531d5f9cab8700f712d410
Wilamis Micael de Araujo Aviz <wmaa@ic.ufal.br>
2023-09-22 13:04:42 -0300

    feat: add seeds

--- src/main/scala/database/DatabaseInitializer.scala
+++ src/main/scala/database/DatabaseInitializer.scala
@@ -1,13 +1,29 @@
 package database
 
+import java.sql.Statement
+
+import DatabaseSeeder.seedInvestmentType
+import DatabaseSeeder.seedBank
+import DatabaseSeeder.seedAccountType
 object DatabaseInitializer {
   def initialize(): Unit = {
     DBConnection.getConnection match {
       case Some(connection) =>
         println("Connection to the database established successfully.")
-        // For example: connection.createStatement().execute("SELECT * FROM your_table")
         DBSetup.initializeDatabase()
-        // connection.close()
+        seedInvestmentType.foreach { 
+          data =>
+            connection.createStatement().execute(s"INSERT INTO investments_types(name) VALUES ('${data.name}')")
+        }
+        seedBank.foreach { 
+          data =>
+          connection.createStatement().execute(s"INSERT INTO banks(name) VALUES ('${data.name}')")
+        }
+        seedAccountType.foreach { 
+          data =>
+          connection.createStatement().execute(s"INSERT INTO accounts_type(name) VALUES ('${data.name}')")
+        }
+        connection.close()
       case None =>
         println("Failed to establish a connection to the database.")
     }
---
+++ src/main/scala/database/Seeds.scala
@@ -0,0 +1,28 @@
+package database
+import model.InvestmentType
+import model.Bank
+import model.AccountType
+
+object DatabaseSeeder{
+  // Seed data
+  val seedInvestmentType = Seq(
+    InvestmentType(None, "Tesouro Direto"),
+    InvestmentType(None, "Poupança"),
+    InvestmentType(None, "CDB"),
+  )
+
+  val seedBank = Seq(
+    Bank(None, "Banco do Brasil"),
+    Bank(None, "Caixa"),
+    Bank(None, "Nubank"),
+    Bank(None, "Inter"),
+    Bank(None, "Will"),
+  )
+
+  val seedAccountType = Seq(
+    AccountType(None, "Corrente"),
+    AccountType(None, "Poupança"),
+    AccountType(None, "Conjunta"),
+    AccountType(None, "Salário"),
+  )
+}
